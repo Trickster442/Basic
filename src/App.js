6c96b9f8cdc7d05059f0798b1d159571c8748d9c
Trickster <reone054@gmail.com>
2024-03-06 23:40:50 +0545

    hero section complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Navbar";
 import './App.css';
+import Hero from "./Components/Hero/Hero";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Item from '../images/shoe_image.png'
+import icon1 from '../images/flipkart.png'
+import icon2 from '../images/amazon.png'
+import './App.css'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+        <div className="hero-btn">
+            <button>Shop Now</button>
+            <button className="secondary-btn">Category</button>
+        </div>
+
+        <div className="hero-avai">
+            <p>Also Available On</p>
+            <div className="icons">
+                <img src={icon1} alt="flipkart"/>
+                <img src={icon2} alt="amazon"/>
+            </div>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src={Item} alt="Item Picture"/>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
